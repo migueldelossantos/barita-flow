c4fbb7f5d57f756e4ea2d8afbed07d7d7d07d088
Miguel de los Santos <migueldelossantosh@gmail.com>
2026-07-21 01:44:40 -0600

    Ajute de  error por impresora

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["escpos", "escpos-usb", "usb"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.supabase.co" },
